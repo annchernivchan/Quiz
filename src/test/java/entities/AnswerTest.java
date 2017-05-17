82eb01da55a9f00fcb16aef51b8f239caccdb42e
AnnChernivchan <annachernivchan@gmail.com>
2017-05-17 13:35:53 +0300

    changed answers tests

--- src/test/java/entities/AnswerTest.java
+++ src/test/java/entities/AnswerTest.java
@@ -21,19 +21,5 @@ public class AnswerTest {
         }
     }
 
-    @Test
-    public void isAnswerWeightRecalculationCorrect () {
-        Task task = new Task();
-
-        task.addQuestion(new Question("Name the spring months", 12, QuestionType.MULTI_CHOICE));
-
-        task.addAnswerForQuestion(task.getQuestions().get(0), new Answer("March", 70));
-        task.addAnswerForQuestion(task.getQuestions().get(0), new Answer("April", 30));
-        task.addAnswerForQuestion(task.getQuestions().get(0), new Answer("May", 21));
-        task.addAnswerForQuestion(task.getQuestions().get(0), new Answer("December", 0));
-
-        Assert.assertEquals(AnswerValidator.isAnswersWeightCorrect(task.getQuestions().get(0).getAllAnswers()), true);
-
-    }
 
 }
